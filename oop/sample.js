0e8129ce9ee0ed0e9e97bfcd15107e322a9d2685
umezawann <yaotomegaku816trigger@gmail.com>
2021-10-06 22:13:42 +0900

    commit

--- oop/sample.js
+++ oop/sample.js
@@ -2,35 +2,34 @@ class Person {
   constructor(first, last, age, gender, interests) {
     this.name = {
       first,
-      last
-    };
-    this.age = age;
-    this.gender = gender;
-    this.interests = interests;
+      last,
+    }
+    this.age = age
+    this.gender = gender
+    this.interests = interests
   }
 
   greeting() {
-    console.log(`Hi! I'm ${this.name.first}`);
-  };
+    console.log(`Hi! I'm ${this.name.first}`)
+  }
 
   farewell() {
-    console.log(`${this.name.first} has left the building. Bye for now!`);
-  };
+    console.log(`${this.name.first} has left the building. Bye for now!`)
+  }
 }
 
 class Teacher extends Person {
   constructor(first, last, age, gender, interests, subject, grade) {
-    super(first, last, age, gender, interests);
+    super(first, last, age, gender, interests)
 
     // 科目と学年は教師によって決まっている
-    this.subject = subject;
-    this.grade = grade;
+    this.subject = subject
+    this.grade = grade
   }
 
   hoge() {
-    console.log(`hogehogehoge`);
-  };
-
+    console.log(`hogehogehoge`)
+  }
 }
 
 // let harry = new Person('Severus', 'Snape', 58, 'male', ['Potions'], 'Dark arts', 5);
--- oop/school.js
+++ oop/school.js
@@ -1,20 +1,22 @@
 class Student {
   // TODO: 名前、大学名、性別、学年
   constructor(name, university, gender, grade) {
-    this.name = name;
-    this.university = university;
-    this.gender = gender;
-    this.grade = grade;
+    this.name = name
+    this.university = university
+    this.gender = gender
+    this.grade = grade
   }
 
   // TODO: 勉強するというメソッド。console.logで値を出力できれば良し
- study() {
-   console.log(`Hello! I'm ${this.grade}year student. I study at ${this.university}`)
- }
+  study() {
+    console.log(
+      `Hello! I'm ${this.grade}year student. I study at ${this.university}`,
+    )
+  }
 
   // TODO: 寝る（eat）というメソッド。睡眠時間を出力する
   sleep(time) {
-    console.log(`I sleep ${time} hours every day`);
+    console.log(`I sleep ${time} hours every day`)
     // console.log('I slept ' + time + ' hours last night')
   }
 }
@@ -22,26 +24,26 @@ class Student {
 // Studentを継承する
 class LiberalArtsStudent extends Student {
   constructor(name, university, gender, grade) {
-    super(name, university, gender, grade);
+    super(name, university, gender, grade)
   }
 
   // TODO: 勉強するというメソッド。console.logで値を出力できれば良し
-  liberalarts () {
+  liberalarts() {
     console.log(`I study liberalarts at ${this.university}`)
-  };
+  }
 
   // TODO: 寝る（eat）というメソッド。睡眠時間を出力する
   eat(food) {
     console.log(`I want to eat ${food} for lunch!`)
-  };
+  }
 }
 
 // Studentを継承する
-class MedicalStudent　extends Student {
+class MedicalStudent extends Student {
   constructor(name, university, gender, grade, age) {
-    super(name, university, gender, grade);
+    super(name, university, gender, grade)
 
-    this.age = age;
+    this.age = age
   }
 
   // TODO: 勉強するというメソッド。console.logで値を出力できれば良し
@@ -55,13 +57,13 @@ class MedicalStudent　extends Student {
   }
 }
 
-let snape = new Student('Snape', 'Tokyo', 'male', 'third');
-let june = new LiberalArtsStudent('June', 'Yamanashi', 'female', 'second');
-let natsu = new MedicalStudent('natsu', 'TWMU', 'female', 'first', '19');
+let snape = new Student('Snape', 'Tokyo', 'male', 'third')
+let june = new LiberalArtsStudent('June', 'Yamanashi', 'female', 'second')
+let natsu = new MedicalStudent('natsu', 'TWMU', 'female', 'first', '19')
 
-snape.study();
-snape.sleep(7);
-june.liberalarts();
-june.eat(sushi);
-natsu.medical();
-natsu.greeting();
+snape.study()
+snape.sleep(7)
+june.liberalarts()
+june.eat(sushi)
+natsu.medical()
+natsu.greeting()
