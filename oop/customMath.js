e53ced21de1e7fafca460335ca37b9ab8ade0385
keisuke ohashi <kog71903@gmail.com>
2021-10-06 22:09:23 +0900

    customMath

---
+++ oop/customMath.js
@@ -0,0 +1,13 @@
+class CustomMath {
+  constructor() {
+  }
+
+  random() {
+    // ランダムな数列を返す
+    // ロジック: 現在の時刻を取得（new Date()） → unix時間に変換→下8桁を返す
+  }
+}
+
+
+const cm = new CustomMath()
+cm.random()
