45fb44f5d9c0d189be32d53e7b5284a96f622c6d
keisuke ohashi <kog71903@gmail.com>
2021-10-07 22:20:29 +0900

    add assignment

---
+++ oop/html.js
@@ -0,0 +1,14 @@
+class CustomHTML {
+  // tag: 'div', 'p', 'li'とか
+  constructor(tag) {
+    // TODO: initializeする
+  }
+
+  generate() {
+    // 課題
+    // 期待する返り値
+    // p -> <p></p>
+    // div -> <div></div>
+    // に変換する
+  }
+}
