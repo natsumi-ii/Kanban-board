0cf7e182babf5e0b8469174f005525754ffe4ac3
umezawann <yaotomegaku816trigger@gmail.com>
2021-10-14 21:52:37 +0900

    途中

--- oop/author.js
+++ oop/author.js
@@ -1,22 +1,36 @@
 class Publisher {
   constructor() {
     // TODO: authors, nameをconstructorの引数にして、propertyに代入する
+    this.authors = this.authors
+    this.name = new Author()
+    console.log('Author is', this.name)
   }
 }
 
 class Author {
-  constructor() {
+  constructor(name, books) {
     // TODO: name, books をconstructorの引数にして、propertyに代入する
-    // this.books
+    this.name = name
+    this.books = books
+    console.log('name is', this.name)
+    console.log('books is', this.books)
   }
 }
 
 class Book {
-  constructor() {
+  constructor(data) {
     // TODO: title, dateをconstructorの引数にして、propertyに代入する
     // this.title = 'hogehoge'
+    // const obj = {}
+    // this.books = data.forEach(data => {
+    //   obj[title] = data
+    // })
+    this.title = data
+    console.log('title is', this.title)
   }
 }
+a
+let a = new Book('book1')
 
 // TODO: publisher, author, bookのデータを複数作り、表示させる。このとき、各データが関連を持つように設定する
 // 出力例
